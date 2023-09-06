215c9b388dd2bb0ffa0d61251bd689de27cdcdc9
MSA <crystalbluetech@gmail.com>
2023-09-06 20:15:35 +0100

    add teacher subclass to also inherit methods from person

--- student.rb
+++ student.rb
@@ -1,10 +1,10 @@
 class Student < Person
-    def initialize(classroomm = 'no classroom', name: 'Unknown', age: nil, parent_permission: true)
-      super(name: name, age: age, parent_permission: parent_permission)
-      @classroomm = classroomm
-    end
-  
-    def play_hooky
-      '¯(ツ)/¯'
-    end
+  def initialize(classroomm = 'no classroom', name: 'Unknown', age: nil, parent_permission: true)
+    super(name: name, age: age, parent_permission: parent_permission)
+    @classroomm = classroomm
   end
+
+  def play_hooky
+    '¯(ツ)/¯'
+  end
+end
