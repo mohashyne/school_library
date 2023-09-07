668d1a8ead01e05e7f63b12a2ccc2c9de9a4f131
MSA <crystalbluetech@gmail.com>
2023-09-07 16:13:57 +0100

    fix error

--- nameable.rb
+++ nameable.rb
@@ -1,6 +1,5 @@
-
 class Nameable
-    def correct_name
-      raise NotImplementedError, 'The correct_name method must be implemented in subclasses.'
-    end
+  def correct_name
+    raise NotImplementedError, 'The correct_name method must be implemented in subclasses.'
+  end
 end
