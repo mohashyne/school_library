6108574ef8c0739b275f64a744438e9ac0fcbe81
MSA <crystalbluetech@gmail.com>
2023-09-08 23:06:43 +0100

    update readme file

--- student.rb
+++ student.rb
@@ -5,7 +5,7 @@ class Student < Person
 
   def initialize(classroom, age, name = 'Unknown', parent_permission: true)
     super(age, name, parent_permission: parent_permission)
-    # @name = name
+    
     @classroom = classroom
     classroom.students.push(self) unless classroom.students.include?(self)
   end
